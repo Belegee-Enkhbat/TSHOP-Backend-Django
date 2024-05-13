e5494355e356d8f0bfa5841111798ba874fc79d5
beeembe <117098491+belgeee@users.noreply.github.com>
2024-05-13 17:56:35 +0800

    oird push hiisengv nileen um zasaw blog bidend hereggv bsn bn del sul um zassan card table nemeh uldsen

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path("articles/", views.ArticlesList.as_view()),
-    path("latest-articles/", views.LatestArticles.as_view()),
-    path("articles/<slug:article_slug>/", views.ArticleDetail.as_view()),
-]
